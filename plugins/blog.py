53cc324b916d81ceac8e7c181ab174ec39ff32cd
Moritz Wilhelmy <moritz+git@wzff.de>
2015-10-18 12:35:19 +0200

    remove headline requirement from blog plugin

--- plugins/blog.py
+++ plugins/blog.py
@@ -34,7 +34,7 @@ def preBuild(site):
 			context_post = {"path": page.path}
 
 			# Check if we have the required keys
-			for field in ["title", "headline", "date", "author"]:
+			for field in ["title", "date", "author"]:
 				
 				if not context.has_key(field):
 					logging.warning("Page %s is missing field: %s" % (page.path, field))
